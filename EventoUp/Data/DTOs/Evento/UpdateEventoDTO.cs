709238c43a46806850f97fbfbd3bd07be0e6ac92
Paulo Neto <paulofrancisco97@gmail.com>
2023-08-04 17:25:56 -0300

    Criando DTOs Update

---
+++ EventoUp/Data/DTOs/Evento/UpdateEventoDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventoUp.Data.DTOs.Evento;
+
+public class UpdateEventoDTO
+{
+    [Required]
+    public string? Nome { get; set; }
+
+    [Required]
+    public string? Local { get; set; }
+
+    [Range(1, 9999)]
+    public int Capacidade { get; set; }
+
+    [Required]
+    public string? Genero { get; set; }
+
+    [Required]
+    public DateTime DataDoEvento { get; set; }
+
+    public string? Descricao { get; set; }
+}
